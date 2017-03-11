0e5dd6747f1613ed674d776ea5be3ba48ce95bcf
Brock Fanning <brockfanning@gmail.com>
2017-03-11 12:12:37 -0500

    Change name of theme to uswds to match name of standards on npm

--- template.php
+++ template.php
@@ -5,7 +5,7 @@
  * Implements hook_preprocess_html().
  *
  */
-function us_web_design_standards_preprocess_html(&$variables) {
+function uswds_preprocess_html(&$variables) {
   // set the path to the theme and make it available in html.tpl.php
-  $variables['theme_path'] = drupal_get_path('theme', 'us_web_design_standards');
+  $variables['theme_path'] = drupal_get_path('theme', 'uswds');
 }
