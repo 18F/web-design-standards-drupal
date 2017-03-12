8795bdab0a89e105004eec4ffa369b21e6585f23
Brock Fanning <brockfanning@gmail.com>
2017-03-11 22:30:23 -0500

    Move scripts before page_bottom, to avoid javascript errors when some 'footer' code tries to use jquery before it is added

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -33,9 +33,8 @@
   <body class="<?php print $classes; ?>" <?php print $attributes;?>>
     <?php print $page_top; ?>
     <?php print $page; ?>
-    <?php print $page_bottom; ?>
-
     <?php print $scripts; ?>
+    <?php print $page_bottom; ?>
   </body>
 
 </html>
